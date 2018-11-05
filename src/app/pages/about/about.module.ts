6d3d26a27094a377e3eb1ee53891c138a1af0eba
Önder Ceylan <onderceylan@gmail.com>
2018-11-05 21:03:04 +0100

    Simplified about page

--- src/app/pages/about/about.module.ts
+++ src/app/pages/about/about.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { AboutPage } from './about';
-import { PopoverPage } from '../about-popover/about-popover';
 import { AboutPageRoutingModule } from './about-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
     AboutPageRoutingModule
   ],
-  declarations: [AboutPage, PopoverPage],
-  entryComponents: [PopoverPage],
+  declarations: [AboutPage],
   bootstrap: [AboutPage],
 })
 export class AboutModule {}
