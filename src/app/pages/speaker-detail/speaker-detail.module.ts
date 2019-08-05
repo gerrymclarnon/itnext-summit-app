3716878c38aca6728a909c830a64f87fecb633d9
Gerry McLarnon <gerry@mclarnonworld.com>
2019-08-05 16:19:51 +0100

    Updated navigation to use routerLink

--- src/app/pages/speaker-detail/speaker-detail.module.ts
+++ src/app/pages/speaker-detail/speaker-detail.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { SpeakerDetailPage } from './speaker-detail';
 import { SpeakerDetailPageRoutingModule } from './speaker-detail-routing.module';
 import { IonicModule } from '@ionic/angular';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     IonicModule,
     SpeakerDetailPageRoutingModule
   ],
--- src/app/pages/speaker-detail/speaker-detail.ts
+++ src/app/pages/speaker-detail/speaker-detail.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ConferenceData } from '../../providers/conference-data';
 import { Observable } from 'rxjs';
 import { openSpeakerTwitter } from '../../utils/social-engagement';
 import { Speaker } from '../../providers/conference.model';
+import {PlayerService} from '../../providers/player-service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Player} from '../../models/Player';
 
 
 @Component({
@@ -15,18 +17,108 @@ export class SpeakerDetailPage {
   speaker$: Observable<Speaker>;
   openSpeakerTwitter = openSpeakerTwitter;
 
+  newListItem: FormGroup;
+  player: any = {};
+  playerId: any;
+  readonly = true;
+
   constructor(
-    private dataProvider: ConferenceData,
+    private playerService: PlayerService,
+    public formBuilder: FormBuilder,
     private router: Router,
     private route: ActivatedRoute
-  ) {}
+  ) {
 
-  ionViewWillEnter() {
-    const speakerId = this.route.snapshot.paramMap.get('speakerId');
-    this.speaker$ = this.dataProvider.getSpeakerById(speakerId);
+    this.playerId = this.route.snapshot.paramMap.get('speakerId');
+    this.readonly = this.playerId !== '';
+
+    if (this.playerId && this.playerId !== '') {
+      this.playerService.getListItem(this.playerId).subscribe((data: any) => {
+        this.player = data;
+
+        if (this.player) {
+          this.newListItem.setValue({
+            title: this.player.title,
+            email: this.player.email
+          });
+        }
+      });
+    } else {
+      this.player = new Player('', '', false);
+    }
+
+    this.newListItem = this.formBuilder.group({
+      title: [this.player.title, Validators.required],
+      email: [this.player.email, Validators.email]
+    });
   }
 
-  goToSessionDetail(session: any) {
-    this.router.navigateByUrl(`app/tabs/(schedule:session/${session.id})`);
+  ionViewWillEnter() {
+    // const speakerId = this.route.snapshot.paramMap.get('speakerId');
+    // this.speaker$ = this.dataProvider.getSpeakerById(speakerId);
+  }
+
+  toggleReadonly() {
+    this.readonly = !this.readonly;
+    // this.setBackButtonText(this.readonly);
+  }
+
+  // TODO: workaround the hidden back button when page refreshed due to nothing in History object!
+  // private setBackButtonText(readonly: boolean) {
+  //   this.zone.run(() => {
+  //     let backButtonText = readonly ? 'Players' : 'Cancel';
+  //     this.viewCtrl.setBackButtonText(backButtonText);
+  //   });
+  // }
+
+  // TODO: move to playerService?
+  save() {
+    if (this.player && this.player.id) {
+      this.updateListItem();
+    } else {
+      this.addListItem();
+    }
+  }
+
+  addListItem() {
+    this.playerService.addListItem(new Player(
+      this.newListItem.value.title,
+      this.newListItem.value.email,
+      false))
+      .then(() => {
+        this.newListItem.reset();
+        this.router.navigateByUrl(
+          `app/tabs/(players:players)`
+        );
+      })
+      .catch((error: any) => console.error(error));
+  }
+
+  updateListItem() {
+    // Replace with 2-way binding?
+    const player: Player = new Player(
+      this.newListItem.value.title,
+      this.newListItem.value.email,
+      false
+    );
+    player.id  = this.player.id;
+
+    this.playerService.updateListItem(player)
+      .then(() => {
+        this.newListItem.reset();
+        this.router.navigateByUrl(
+          `app/tabs/(players:players)`
+        );            })
+      .catch((error: any) => console.error(error));
+  }
+
+  removeListItem() {
+    this.playerService.removeListItem(this.player)
+      .then(() => {
+        this.router.navigateByUrl(
+          `app/tabs/(players:players)`
+        );
+      })
+      .catch((error: any) => console.error(error));
   }
 }
